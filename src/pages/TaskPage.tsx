deb722869c77eb4ce1e90e3662cb56a78b8c66b8
moyanoerikdemian <moyanoerik1991@gmail.com>
2026-09-20 18:21:56 -0300

    add: sort tasks by deadline in TaskPage component

--- src/pages/TaskPage.tsx
+++ src/pages/TaskPage.tsx
@@ -1,14 +1,15 @@
 import { AddTask } from "../components/AddTask.tsx";
 import { TaskItem } from "../components/TaskItem.tsx";
 import { useTasks } from "../hooks/useTasks.ts";
+import { sortTasksByDeadline } from "../utils/sortTasksByDeadline.ts";
 
 export const TaskPage = () => {
     const { tasks, handleAddTask, handleToggleComplete, handleDelete } = useTasks();
-
+    const sortedTasks = sortTasksByDeadline(tasks);
     return (
         <>
             <AddTask onAddTask={handleAddTask} />
-            {tasks.map((task) => (
+            {sortedTasks.map((task) => (
                 <TaskItem
                     key={task.id}
                     task={task}
