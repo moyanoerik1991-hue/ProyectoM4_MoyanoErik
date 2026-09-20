d68a4d2b3265daab0218099006cf0b142a4c033d
moyanoerikdemian <moyanoerik1991@gmail.com>
2026-09-20 00:30:54 -0300

    add: enhance TaskItem component to display countdown and formatted dates

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,5 +1,7 @@
 import type { Task } from "../types/Task";
 import { Button } from "./ui/Button";
+import { formatDate } from "../utils/formatDate";
+import { useCountdown } from "../hooks/useCountdown";
 
 interface TaskItemProps {
     task: Task;
@@ -8,13 +10,20 @@ interface TaskItemProps {
 }
 
 export const TaskItem = ({ task, onToggleComplete, onDelete }: TaskItemProps) => {
+    const countdown = useCountdown(task.deadline, task.completed);
+
+    let countdownText = null;
+    if (!task.completed) {
+        countdownText = countdown.expired ? "Tiempo Expirado" : `Tiempo Restante: ${countdown.days}d  ${countdown.hours}h  ${countdown.minutes}m`;
+    }
     return (
         <div>
             <h3>{task.title}</h3>
             <p>{task.description}</p>
-            <p>{task.date.toLocaleDateString()}</p>
+            <p>Fecha de creación: {formatDate(task.date)}</p>
+            <p>Fecha de vencimiento: {formatDate(task.deadline)}</p>
             <p>{task.completed ? "Completada" : "Pendiente"}</p>
-
+            {countdownText && <p>{countdownText}</p>}
             <Button label={task.completed ? "Pendiente" : "Completar"} onClick={() => onToggleComplete(task.id)} />
             <Button label="Eliminar" onClick={() => onDelete(task.id)} />
         </div>
