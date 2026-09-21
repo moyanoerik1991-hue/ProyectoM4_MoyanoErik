25f07f417fc81876c1797171511180e1e136e1f3
moyanoerikdemian <moyanoerik1991@gmail.com>
2026-09-20 22:32:28 -0300

    add: implement handleEditTask and update comments for task management functions

--- src/hooks/useTasks.ts
+++ src/hooks/useTasks.ts
@@ -5,6 +5,7 @@ export const useTasks = () => {
     const [tasks, setTasks] = useState<Task[]>([]);
     const nextId = useRef(1);
 
+    // Función para añadir una nueva tarea - se utiliza "lazy initial state" en el useState
     const handleAddTask = (newTask: Omit<Task, "id" | "date" | "completed">) => {
         const task: Task = {
             ...newTask,
@@ -15,6 +16,16 @@ export const useTasks = () => {
         setTasks([...tasks, task]);
     };
 
+    // Función para editar una tarea existente - se utiliza el spred operator para actualizar solo los campos necesarios
+    const handleEditTask = (id: number, updateFields: Omit<Task, "id" | "date" | "completed">) => {
+        setTasks(
+            tasks.map((task) =>
+                task.id === id ? { ...task, ...updateFields } : task
+            )
+        )
+    }
+
+    // Función para alternar el estado de completado de una tarea.
     const handleToggleComplete = (id: number) => {
         setTasks(
             tasks.map((task) =>
@@ -23,6 +34,7 @@ export const useTasks = () => {
         );
     };
 
+    // Función para eliminar una tarea - filter se utiliza para crear un nuevo array que excluye la tarea con el id especificado
     const handleDelete = (id: number) => {
         setTasks(tasks.filter((task) => task.id !== id));
     };
@@ -30,6 +42,7 @@ export const useTasks = () => {
     return {
         tasks,
         handleAddTask,
+        handleEditTask,
         handleToggleComplete,
         handleDelete,
     };
