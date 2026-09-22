6477edbbbf8b9fd7e23577c6c435adb6dc3c4875
moyanoerikdemian <moyanoerik1991@gmail.com>
2026-09-22 20:11:41 -0300

    add: implement routing and authentication in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { NavBar } from "./layout/Navbar.tsx";
 import { TaskPage } from "./pages/TaskPage.tsx"
+import { HomePage } from "./pages/HomePage.tsx";
+import { AboutPage } from "./pages/AboutPage.tsx";
+import { useAuth } from "./hooks/useAuth.ts";
+import { ProtectedRoute } from "./components/auth/ProtectedRoute.tsx";
+
+
 
 export const App = () => {
+    const { isAuthenticated } = useAuth();
+    console.log("Estado de autenticación:", isAuthenticated);
     return (
-        <TaskPage />
+        <BrowserRouter>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/tasks" element={<ProtectedRoute><TaskPage /></ProtectedRoute>} />
+                <Route path="/about" element={<AboutPage />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
