127622881fb27491d63c8932bc8be9959b11c24e
moyanoerikdemian <moyanoerik1991@gmail.com>
2026-09-23 10:59:03 -0300

    fix: add missing newline at end of Task.ts file

--- src/types/Task.ts
+++ src/types/Task.ts
@@ -3,6 +3,7 @@ export type Task = {
     userId: string;
     title: string;
     description: string;
+    category: "Trabajo" | "Hogar" | "Salud" | "Deporte" | "Otros";
     date: Date;
     deadline: Date;
     completed: boolean;
