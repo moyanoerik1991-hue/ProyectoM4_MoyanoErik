2e811084dae4f37a4de740e5e9f667bf5a480c61
moyanoerikdemian <moyanoerik1991@gmail.com>
2026-09-23 01:41:46 -0300

    add: extend Input component to support email and number types with min/max attributes

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -2,16 +2,18 @@ type InputProps = {
     label: string;
     value: string;
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-    type?: "text" | "date" | "time";
+    type?: "text" | "date" | "time" | "email" | "number";
     placeholder?: string;
     maxLength?: number;
+    min?: string;
+    max?: string;
 };
 
-export const Input = ({ label, value, onChange, type = "text", placeholder, maxLength }: InputProps) => {
+export const Input = ({ label, value, onChange, type = "text", placeholder, maxLength, min, max }: InputProps) => {
     return (
         <>
             <label>{label}</label>
-            <input placeholder={placeholder} type={type} value={value} onChange={onChange} maxLength={maxLength} />
+            <input placeholder={placeholder} type={type} value={value} onChange={onChange} maxLength={maxLength} min={min} max={max} />
         </>
     );
 };
