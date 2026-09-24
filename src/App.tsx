ba7d426011e6f83d6220e7963718b818e7262954
moyanoerikdemian <moyanoerik1991@gmail.com>
2026-09-24 02:17:41 -0300

    refactor: reorganize imports and remove unused AboutPage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { NavBar } from "./layout/Navbar.tsx";
-import { TaskPage } from "./pages/TaskPage.tsx"
 import { HomePage } from "./pages/HomePage.tsx";
-import { AboutPage } from "./pages/AboutPage.tsx";
-import { useAuth } from "./hooks/useAuth.ts";
+import { AuthPage } from "./pages/AuthPage.tsx";
+import { TaskPage } from "./pages/TaskPage.tsx";
+
 import { ProtectedRoute } from "./components/auth/ProtectedRoute.tsx";
-
-
+import { PublicOnlyRoute } from "./components/auth/PublicOnlyRoute.tsx";
 
 export const App = () => {
-    const { isAuthenticated } = useAuth();
-    console.log("Estado de autenticación:", isAuthenticated);
     return (
         <BrowserRouter>
             <NavBar />
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="/auth" element={<PublicOnlyRoute><AuthPage /></PublicOnlyRoute>} />
                 <Route path="/tasks" element={<ProtectedRoute><TaskPage /></ProtectedRoute>} />
-                <Route path="/about" element={<AboutPage />} />
             </Routes>
         </BrowserRouter>
     );
-}
+};
